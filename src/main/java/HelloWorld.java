fdd9597a5cfebf9b7186f331b7a65aa47722ad31
Sneha-Sharma <2022wa86978@wilp.bits-pilani.ac.in>
2026-09-24 18:54:01 +0530

    Modified java file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
+	   
         System.out.println("Hello Jenkins CI/CD");
+        System.out.println("Perform continuous integration through Jenkins by modifying source code in Git Repository . ");
     }
 }
